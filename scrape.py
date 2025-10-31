afc8012206d5d6c36e530f90106106d3779d7487
nandanasheri <nandanasheri@gmail.com>
2025-10-31 00:19:55 -0400

    scraped all products

--- scrape.py
+++ scrape.py
@@ -1,7 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://incidecoder.com/products/the-ordinary-niacinamide-10-zinc-1"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
-print(soup)
+'''
+Scrape Plan
+Scrape 400 - 500 products
+each product
+{
+"docid":
+"title":
+"brand":
+"desc":
+"key_ingredient_func" : [list of IDs]
+"other_ingredient_func" : []
+"image_url" : 
+}
+
+each ingredient function
+{
+"func_id":
+"title":
+"desc":
+}
+Scraping about 60-70 pages will get us approximately 400 unique products
+'''
+def scrape_products(pages:int):
+    exclude_links = set(["/products/new", "/products/create"])
+    all_product_links = set()
+
+    for i in range(1, pages+1):
+        url = f"https://incidecoder.com/products?page={i}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        product_links = soup.find_all('a')        
+        for a in product_links:
+            href = a.get("href")  # or a['href']
+            if href.startswith("/products/") and href not in exclude_links and "discontinued" not in href:
+                all_product_links.add(href)
+    
+    return all_product_links
+
+def scrape_each_product(product_links:list):
+    for link in product_links:
+        url = f"https://incidecoder.com{link}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        print(soup)
+
+# products = scrape_products(30)
+scrape_each_product(["/products/ultrasun-anti-age-spf-50"])
